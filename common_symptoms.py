6d6b3fca6f3a59ad897a7284c7fed48385adadb7
sarah <sarah.annealice@gmail.com>
2023-10-27 13:31:13 -0300

    added symptoms buttons

--- common_symptoms.py
+++ common_symptoms.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-def symptoms(root, frame):
+def symptoms_layout(root, frame):
     frame.pack_forget()
     root.title("Common Symptoms")
     root.configure(bg="light blue")
@@ -14,11 +14,38 @@ def symptoms(root, frame):
 
     lbl_symptopms = tk.Label(
         master=frm_symptoms,
-        text=("Common Symptoms"),
+        text="Common Symptoms",
         fg="navy",
-        font=("Arial", 18),
+        font=("Arial", 18)
     )
-    lbl_symptopms.grid(row=0, column=0, columnspan=2)
+    lbl_symptopms.grid(row=0, column=0, columnspan=3)
+
+    btn_heater = tk.Button(
+        master=frm_symptoms,
+        text="Heater not working",
+        fg="white",
+        bg="navy",
+        font=14
+    )
+
+    btn_ac = tk.Button(
+        master=frm_symptoms,
+        text="A/C not working",
+        fg="white",
+        bg="navy",
+        font=14
+    )
+    
+    btn_oil_leak = tk.Button(
+        master=frm_symptoms,
+        text="Car leaking oil",
+        fg="white",
+        bg="navy",
+        font=14
+    )
+    btn_heater.grid(row=1, column=0, padx=5, pady=5)
+    btn_ac.grid(row=1, column=1, padx=5, pady=5)
+    btn_oil_leak.grid(row=1, column=2, padx=5, pady=5)
 
     btn_menu = tk.Button(
         master=frm_symptoms,
@@ -29,8 +56,14 @@ def symptoms(root, frame):
     )
     btn_menu.grid(row=2, column=0, columnspan=2)
 
+
+
+
 def menu(root, frame, frm_symptoms):
     frm_symptoms.pack_forget()
-    frame.pack()
+    frame.pack(side=tk.TOP)
     root.title("CarMD")
     root.configure(bg="pink")
+
+
+def heater_broken(root):
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 #CarMD main page
 import tkinter as tk
 import tkinter.ttk as ttk
-import requests
-from bs4 import BeautifulSoup
 
-from common_symptoms import symptoms
+from common_symptoms import symptoms_layout
 
 root = tk.Tk()
 root.title("CarMD")
@@ -17,7 +15,7 @@ frame = tk.Frame(
     padx=10,
     pady=10,
 )
-frame.pack()
+frame.pack(side="top")
 
 label = tk.Label(
     master=frame,
@@ -33,7 +31,7 @@ btn_left = tk.Button(
     bg="blue",
     fg="yellow",
     font=16,
-    command=lambda: symptoms(root, frame)
+    command=lambda: symptoms_layout(root, frame)
 )
 btn_left.grid(row=1, column=0)
 
@@ -45,24 +43,5 @@ btn_right = tk.Button(
     font=16
 )
 btn_right.grid(row=1, column=1)
-
-URL = "https://realpython.github.io/fake-jobs/"
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(id="ResultsContainer")
-
-job_elements = results.find_all("div", class_="card-content")
-for job_element in job_elements:
-    title_element = job_element.find("h2", class_="title")
-    company_element = job_element.find("h3", class_="company")
-    location_element = job_element.find("p", class_="location")
-    print(title_element.text.strip())
-    print(company_element.text.strip())
-    print(location_element.text.strip())
-    print("\n")
-
-
-# print(results.prettify())
 
 root.mainloop()
