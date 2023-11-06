a8ad1a6c0b2c5888e4ab88e40008778d4a4febb6
sarah <sarah.annealice@gmail.com>
2023-11-06 11:22:29 -0400

    testing different layouts

--- common_symptoms.py
+++ common_symptoms.py
@@ -1,5 +1,58 @@
 import tkinter as tk
 
+# ----------symptom windows---------- #
+def heater_broken(root, frame):
+    frame.pack_forget()
+    root.title("Heater not Working")
+    root.configure(bg="light yellow")
+
+    frm_heater = tk.Frame(
+        master=root,
+        padx=10,
+        pady=10,
+    )
+    frm_heater.pack()
+
+    lbl_title = tk.Label(
+        master=frm_heater,
+        text="How a heating system works")
+    lbl_title.grid(row=0, column=0, columnspan=4)
+
+    # insert canvas with scrollbar here for info
+    lbl_info = tk.Label(
+        master=frm_heater,
+        text="A vehicle's heating system is also part of its cooling system.\n"
+    + "The coolant is circulated through the engine in which it absorb heat,\n "
+    + "is sent to the radiator where it dissipates. This heated coolant can be expelled through the dashboard\n "
+    + "if the heater is turned on."
+    )
+    lbl_info.grid(row=1, column=1, columnspan=2)
+
+    # insert beautiful soup web scraping here for cost to repair
+
+def ac_broken():
+    win_ac = tk.Toplevel()
+    win_ac.geometry("500x300")
+    win_ac.configure(bg="light yellow")
+    win_ac.wm_title("Heater not Working")
+
+    lbl_title = tk.Label(
+        master=win_ac,
+        text="How a heating system works")
+    lbl_title.grid(row=0, column=0, columnspan=4)
+
+    # insert canvas with scrollbar here for info
+    lbl_info = tk.Label(
+        master=win_ac,
+        text="A vehicle's heating system is also part of its cooling system.\n"
+    + "The coolant is circulated through the engine in which it absorb heat,\n "
+    + "is sent to the radiator where it dissipates. This heated coolant can be expelled through the dashboard\n "
+    + "if the heater is turned on."
+    )
+    lbl_info.grid(row=1, column=1, columnspan=2)
+
+  
+# ----------main layout---------- #
 def symptoms_layout(root, frame):
     frame.pack_forget()
     root.title("Common Symptoms")
@@ -26,7 +79,7 @@ def symptoms_layout(root, frame):
         fg="white",
         bg="navy",
         font=14,
-        command=heater_broken
+        command=lambda:heater_broken(root, frame)
     )
 
     btn_ac = tk.Button(
@@ -34,7 +87,8 @@ def symptoms_layout(root, frame):
         text="A/C not working",
         fg="white",
         bg="navy",
-        font=14
+        font=14,
+        command=ac_broken
     )
     
     btn_oil_leak = tk.Button(
@@ -58,34 +112,8 @@ def symptoms_layout(root, frame):
     btn_menu.grid(row=2, column=0, columnspan=2)
 
 
-
 def menu(root, frame, frm_symptoms):
     frm_symptoms.pack_forget()
     frame.pack(side=tk.TOP)
     root.title("CarMD")
     root.configure(bg="pink")
-
-
-def heater_broken():
-    win = tk.Toplevel()
-    win.geometry("500x300")
-    win.configure(bg="light yellow")
-    win.wm_title("Heater not Working")
-
-    lbl_title = tk.Label(
-        master=win,
-        text="How a heating system works")
-    lbl_title.grid(row=0, column=0, columnspan=4)
-
-    # insert canvas with scrollbar here for info
-    lbl_info = tk.Label(
-        master=win,
-        text="A vehicle's heating system is also part of its cooling system.\n"
-    + "The coolant is circulated through the engine in which it absorb heat,\n "
-    + "is sent to the radiator where it dissipates. This heated coolant can be expelled through the dashboard\n "
-    + "if the heater is turned on."
-    )
-    lbl_info.grid(row=1, column=1, columnspan=2)
-
-    # insert beautiful soup web scraping here for cost to repair
-    # this is a test
